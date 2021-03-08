a4a5c113708b31da65ae41f2660edf24c4af6178
vinicius95vnc <xvncxx@gmail.com>
2021-03-08 14:22:30 -0300

    Adicionar arquivos de projeto.

---
+++ Sistema de cadastro/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_cadastro
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public string DataNascimeto { get; set; }
+        public string EstadoCivil { get; set; }
+        public string Telefone { get; set; }
+        public bool CasaPropria { get; set; }
+        public bool Veiculo { get; set; }
+        public char Sexo { get; set; }
+
+    }
+}
